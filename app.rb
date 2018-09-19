f75ff895af1902d605a1a0f35fe3b2a89940b9df
Malcome Fein <mfein90@gmail.com>
2018-09-19 17:46:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,6 +6,18 @@ class App < Sinatra::Base
     erb :food_form
   end
 
+  get '/' do
+   "Hello World"
+ end
+
   # Add your post route and action below
+  post '/food' do
+    # params.to_s
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+
+
+
+
 
 end
